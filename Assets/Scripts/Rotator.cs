80edb0390ab2a90c06bb180b70fbdd9bc32bdc7e
Victor Hong <vhoncito@gmail.com>
2024-10-31 15:40:42 +0000

    rotating pickup

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
 
-public class Rotator
+public class Rotator : MonoBehaviour
 {
-    
+    // Erased Start method, don't needed.
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+
+        
+    }
 }
