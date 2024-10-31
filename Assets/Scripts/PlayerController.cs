866bf7142869c27f9d3d29e6893495ca88ed0426
Victor Hong <vhoncito@gmail.com>
2024-10-31 15:26:08 +0000

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour 
+{
+
+    public float speed = 10.0f;
+    private Rigidbody rb;
+
+    // Holds movement x and y
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+        rb = GetComponent<Rigidbody>(); // Sets rigibody component to rb
+        
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        
+        // Create a vector 2 variable and store the x and y movement values in it
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        // Set the movement to the x and z variables (keep y at 0)
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+        
+    }
+
+}
